136dc5e563c34bc7d097272bd8ed28aea0bdbdf2
Anshul Jain <anjoy04@gmail.com>
2016-12-24 14:57:49 +0530

    soundcloud embedding feature

--- h/static/scripts/annotator/plugin/toolbar.coffee
+++ h/static/scripts/annotator/plugin/toolbar.coffee
@@ -79,14 +79,60 @@ module.exports = class Toolbar extends Annotator.Plugin
           event.preventDefault()
           event.stopPropagation()
           uri = document.location.href
-          console.log (uri)
-          match = uri.includes("youtube.com")
-          console.log(match)
+          matchYT = uri.includes("youtube.com")
+          matchSC = uri.includes("soundcloud.com")
           renoted_id = new Date().getTime().toString() + Math.floor((Math.random() * 10000) + 1).toString();
           val={}
-          if not match
-              alert("only works with youtube for now")
-          else
+
+          
+          if matchSC
+              console.log("inside SoundCloud")
+              scDomainURI = "https://soundcloud.com"
+              scPlayer=document.getElementsByClassName("playbackTimeline__progressWrapper")
+
+              # Getting the URI from the player as continuous playback leads to 
+              # mismatch in soundtrack URI and Tab URI
+              scPlayback = document.getElementsByClassName("playbackSoundBadge")
+              scPlayerLink = scPlayback[0].getElementsByTagName("a")
+              scPlayerURI = scPlayerLink[0].getAttribute("href")
+              scTitleElems = scPlayback[0].getElementsByClassName("playbackSoundBadge__title sc-truncate")
+              scPlayerTitle = scTitleElems[0].getAttribute("title")
+            
+              if not scPlayer.length > 0
+                alert "No sound playing"
+              else
+                if not IDLIST.length > 0
+                  starttime=scPlayer[0].getAttribute('aria-valuenow')
+                  val.id =renoted_id
+                  val.starttime=starttime
+                  val.uri = scDomainURI + scPlayerURI
+                  val.title = scPlayerTitle
+                  IDLIST.push(val) 
+                  console.log(IDLIST)
+                  console.log("+++++starttime+++++")
+                  console.log(starttime)
+                  state = true
+                  this.setVideoSnippetButton state
+
+                else
+                  newURI = scDomainURI + scPlayerURI
+
+                  if IDLIST[0].uri != newURI
+                    alert("Cannot create annotation as the sound has changed from the time the capture was started!")
+                    
+                  else
+                    endtime=scPlayer[0].getAttribute('aria-valuenow')
+                    console.log("+++++endtime+++++")
+                    console.log(endtime)
+                    IDLIST[0].endtime=endtime
+                    console.log(IDLIST)
+                    @annotator.createAnnotation($renoted_id : IDLIST[0].id, auddata: IDLIST)
+                  
+                  IDLIST=[]
+                  state = false
+                  this.setVideoSnippetButton state
+
+          else if matchYT
               ytplayer=document.getElementById("movie_player")
               
               console.log("already on youtube")
@@ -113,7 +159,9 @@ module.exports = class Toolbar extends Annotator.Plugin
                    console.log(starttime)
                    state = true
                    this.setVideoSnippetButton state
-              
+          else
+              alert("only works with Youtube and SoundCloud for now")
+
 #          @annotator.createAnnotation($renoted_id : "hel1234")
 #          @annotator.show()
 #
--- h/static/scripts/app.js
+++ h/static/scripts/app.js
@@ -194,6 +194,129 @@ module.exports = angular.module('h', [
   .directive('timestamp', require('./directive/timestamp'))
   .directive('topBar', require('./directive/top-bar'))
   .directive('windowScroll', require('./directive/window-scroll'))
+  .directive('soundcloudTrack', ['soundcloudWidgetUtils', '$compile', function(soundcloudWidgetUtils, $compile) {
+  
+    var uniqId = 1;
+  
+    var eventPrefix = "soundcloud.player.";
+  
+    return {
+      restrict: 'A',
+
+      scope: {
+        trackUrl: '=?',
+        player: '=?',
+        playerVars: '=?' 
+      },
+      transclude: true,
+
+      link: function(scope, element, attrs) {
+        console.log("inside the sc link function");
+
+        // allows us to $watch `ready`
+        scope.utils = soundcloudWidgetUtils;
+
+        // player-id attr > id attr > directive-generated ID
+        var playerId = scope.playerId || element[0].id || 'unique-soundcloud-widget-id-' + uniqId++;
+        element[0].id = playerId;
+
+        console.log("Player ID: "+ playerId)
+
+        var serialize = function(obj) {
+          var str = [];
+          for(var p in obj)
+            if (obj.hasOwnProperty(p)) {
+              str.push(p + "=" + obj[p]);
+            }
+          return str.join("&");
+        };
+
+        // Defaults for variables
+        scope.playerVars = scope.playerVars || {};
+      
+        function createPlayer() {
+          
+          console.log("inside sc createPlayer function");      
+          var player_events = [
+            SC.Widget.Events.LOAD_PROGRESS,
+            SC.Widget.Events.PLAY_PROGRESS,
+            SC.Widget.Events.PLAY,
+            SC.Widget.Events.PAUSE,
+            SC.Widget.Events.FINISH,
+            SC.Widget.Events.SEEK,
+            SC.Widget.Events.READY,
+            SC.Widget.Events.CLICK_DOWNLOAD,
+            SC.Widget.Events.CLICK_BUY,
+            SC.Widget.Events.OPEN_SHARE_PANEL,
+            SC.Widget.Events.ERROR
+          ];
+        
+          var iframe = document.createElement('iframe');
+          iframe.src = "https://w.soundcloud.com/player/?url=" + scope.trackUrl + "&" + serialize(scope.playerVars);
+          iframe.id = playerId;
+          element.empty().append($compile(iframe)(scope));
+          var player = SC.Widget(iframe);
+          player.id = playerId;
+        
+          player_events.forEach(function(event) {
+            player.bind(event, function(){
+              var args = Array.prototype.slice.call(arguments);
+              args.unshift(eventPrefix + event, scope.player);
+              scope.$emit.apply(scope, args);
+            });
+          });
+        
+          return player;
+        };
+      
+        function loadPlayer () {
+          console.log("inside loadPlayer function()");
+          if(playerId && scope.trackUrl) {
+            scope.player = createPlayer();
+          }
+        }
+      
+        var stopWatchingReady = scope.$watch(
+          function () {
+            return scope.utils.ready && scope.trackUrl !== undefined;
+          },
+          function (ready) {
+            if(ready) {
+              stopWatchingReady();
+              scope.$watch('trackUrl', function(url) {
+                console.log("loading the sc player");
+                loadPlayer();
+              });
+            }
+          }
+        );
+
+      }
+    };
+  }])
+/*
+  .directive('scWidgetScriptLoad', function($window) {
+  return {
+    restrict: "E",
+
+    scope: {
+      height: "@",
+      width: "@",
+      videoid: "@"
+    },
+
+    templateUrl: "scTemplate.html",
+
+    link: function(scope, element) {
+      var tag = document.createElement('script');
+      tag.src = "https://w.soundcloud.com/player/api.js";
+      var firstScriptTag = document.getElementsByTagName('script')[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+      console.log("Injecting the script for SC");
+    }  
+  };  
+  }) 
+*/
   .directive('youtube', function($window) {
   return {
     restrict: "E",
@@ -255,6 +378,7 @@ module.exports = angular.module('h', [
   };
 })
 
+
   .service('annotationMapper', require('./annotation-mapper'))
   .service('annotationUI', require('./annotation-ui'))
   .service('auth', require('./auth').service)
@@ -279,6 +403,38 @@ module.exports = angular.module('h', [
   .service('tags', require('./tags'))
   .service('unicode', require('./unicode'))
   .service('viewFilter', require('./view-filter'))
+  .service ('soundcloudWidgetUtils', ['$rootScope', function ($rootScope) {
+
+    var Service = {};
+  
+    Service.ready = false;
+
+    (function () {
+
+      var validProtocols = ['http:', 'https:'];
+      var url = 'https://w.soundcloud.com/player/api.js';
+
+      // We'd prefer a protocol relative url, but let's
+      // fallback to `http:` for invalid protocols
+      //if (validProtocols.indexOf(window.location.protocol) < 0) {
+      //  url = 'http:' + url;
+      //}
+      var tag = document.createElement('script');
+      tag.src = url;
+      tag.onload = function() {
+          $rootScope.$apply(function () {
+            Service.ready = true;
+          });
+      };
+    
+      var firstScriptTag = document.getElementsByTagName('script')[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+      console.log("succesfully injected the SC API");
+    }());
+  
+    return Service;
+
+  }])
 
   .factory('store', require('./store'))
 

579fbf13373deaaed79936f0b5b7219625bbf8da
akash1808 <akash.agrawal1992@gmail.com>
2017-02-20 14:10:27 +0530

    changes for css on top bar selection

--- h/static/scripts/directive/top-bar.js
+++ h/static/scripts/directive/top-bar.js
@@ -18,6 +18,7 @@ module.exports = function () {
       accountDialog: '<',
       shareDialog: '<',
       sortKey: '<',
+      selectedUrlFilterKey: '<',
       sortKeysAvailable: '<',
       onChangeSortKey: '&',
       setUrlFilterKeyAll: '&',
